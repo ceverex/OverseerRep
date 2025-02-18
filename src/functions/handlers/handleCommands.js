31da55884625047ce02c23dae7a797fef6473a30
Henry-Epic <90076290+Henry-Epic@users.noreply.github.com>
2025-02-18 06:49:10 -0800

    new bot

---
+++ src/functions/handlers/handleCommands.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+module.exports = (client) => {
+  client.handleCommands = async () => {
+    const commandFolders = fs.readdirSync("./src/commands");
+    for (const folder of commandFolders) {
+      const commandFiles = fs
+        .readdirSync(`./src/commands/${folder}`)
+        .filter((file) => file.endsWith(".js"));
+    }
+  };
+};
