49a510f4c3f283855547e2bb9477df10472ded36
henry <90076290+ceverex@users.noreply.github.com>
2025-02-18 09:07:28 -0800

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 
 const client = Client({ intents: GatewayIntentBits.Guilds });
 client.commands = new Collection();
+client.commandArray = [];
 
 const functionFolders = fs.readdirSync(`./src/functions`);
 for (const folder of functionFolders) {
