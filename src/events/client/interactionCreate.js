58ca520ea364c2110c063f3c6415b3efca3c19d7
Henry-Epic <90076290+Henry-Epic@users.noreply.github.com>
2025-02-18 21:31:59 -0800

    updated interactionCreate

--- src/events/client/interactionCreate.js
+++ src/events/client/interactionCreate.js
@@ -1,3 +1,5 @@
+const { InteractionType, EmbedBuilder } = require("discord.js");
+
 module.exports = {
   name: "interactionCreate",
   async execute(interaction, client) {
@@ -11,10 +13,66 @@ module.exports = {
         await command.execute(interaction, client);
       } catch (error) {
         console.error(error);
+
+        // const ceverexError = client.emojis.cache.find(
+        //   (emoji) => (emoji.id = "1010038746927874078")
+        // );
+
+        const errorEmbed = new EmbedBuilder().setColor(0xFF0000).addFields([
+          {
+            name: `Error Occured`,
+            value: `Something went wrong while executing this command.`,
+          },
+        ]);
+
         await interaction.reply({
-          content: `Something went wrong while executing this command.`,
+          embeds: [errorEmbed],
           ephemeral: true,
         });
+      }
+    } else if (interaction.isButton()) {
+      const { buttons } = client;
+      const { customId } = interaction;
+      const button = buttons.get(customId);
+      if (!button) return new Error(`There is no code for this button.`);
+
+      try {
+        await button.execute(interaction, client);
+      } catch (err) {
+        console.error(err);
+      }
+    } else if (interaction.isSelectMenu()) {
+      const { selectMenus } = client;
+      const { customId } = interaction;
+      const menu = selectMenus.get(customId);
+      if (!menu) return new Error(`There is no code for this select menu.`);
+
+      try {
+        await menu.execute(interaction, client);
+      } catch (error) {
+        console.error(error);
+      }
+    } else if (interaction.type == InteractionType.ModalSubmit) {
+      const { modals } = client;
+      const { customId } = interaction;
+      const modal = modals.get(customId);
+      if (!modal) return new Error(`There is no code for this modal.`);
+
+      try {
+        await modal.execute(interaction, client);
+      } catch (error) {
+        console.error(error);
+      }
+    } else if (interaction.isContextMenuCommand()) {
+      const { commands } = client;
+      const { commandName } = interaction;
+      const contextCommand = commands.get(commandName);
+      if (!contextCommand) return;
+
+      try {
+        await contextCommand.execute(interaction, client);
+      } catch (error) {
+        console.error(error);
       }
     }
   },
