febeecd50dab8f789b7e907f2fb4e6ba06ab5a6d
Henry-Epic <90076290+Henry-Epic@users.noreply.github.com>
2025-02-18 17:14:36 -0800

    bug fix

--- src/bot.js
+++ src/bot.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const { token } = process.env;
-const { Client, Collection, GatewayIntentBits } = require('discord.js');
-const fs = require('fs');
+const { Client, Collection, GatewayIntentBits } = require("discord.js");
+const fs = require("fs");
 
 const client = new Client({ intents: GatewayIntentBits.Guilds });
 client.commands = new Collection();
@@ -9,11 +9,11 @@ client.commandArray = [];
 
 const functionFolders = fs.readdirSync(`./src/functions`);
 for (const folder of functionFolders) {
-    const functionFiles = fs
+  const functionFiles = fs
     .readdirSync(`./src/functions/${folder}`)
-    .filter((file) => file.endsWith('.js'));
-    for (const file of functionFiles) 
-        require(`./functions/${folder}/${file}`)(client);
+    .filter((file) => file.endsWith(".js"));
+  for (const file of functionFiles)
+    require(`./functions/${folder}/${file}`)(client);
 }
 
 client.handleEvents();
